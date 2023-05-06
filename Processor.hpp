c0f348a4a93a923c183c908678954f18393b6f6c
Danep1 <epikman9457@gmail.com>
2023-05-06 23:38:23 +0300

    lattise is kina working, but it needs to be optimised

--- Processor.hpp
+++ Processor.hpp
@@ -11,16 +11,16 @@ private:
 	const float m_width = 1.0f;
 	const float m_height = 1.0f;
 	
-	const float m_v_max = 0.001f;
+	const float m_v_max = 0.0001f;
 
 	const std::size_t m_N_particls_in_row = 5U;
 
-	const float m_sigma = m_length / m_N_particls_in_row / 1.1224620f * 0.5f;
+	const float m_sigma = m_length / m_N_particls_in_row / 1.095f / 2.0f;
 	const float m_R_cut = m_sigma * 2.5f;
 	const float m_U_0 = 1.0f;
 
-	const float t = 3.0e-4;
-	const float dt = 1.0e-5;
+	const float t = 1.0e-3;
+	const float dt = 1.0e-4;
 	const std::size_t m_N_steps = t / dt;
 	const std::size_t m_N_update = 1U;
 
